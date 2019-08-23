c56dc9e5ec6e49b1632354a1299858b636e8370b
Oscar Castro <oscar.castro@pubnubs-MacBook-Air.local>
2019-08-23 14:00:09 -0700

    Added logic so user can delete items for users, spaces and memberships in the browser

--- objects.js
+++ objects.js
@@ -1,13 +1,14 @@
-// Player enters lobby name and game starts once two players are in the same lobby
 (function() {
-let userInput = document.getElementById("user-input"), spaceInput = document.getElementById("space-input"),
-  memberInputUsername = document.getElementById("member-input-username"), memberInputSpacename = document.getElementById("member-input-spacename"), 
-  userButton =  document.getElementById("user-button"), spaceButton =  document.getElementById("space-button"), 
-  memberButton =  document.getElementById("member-button"), userBox =  document.getElementById("user-box"), spaceBox =  document.getElementById("space-box"),
-  memberBox =  document.getElementById("member-box"), userRemoveButton =  document.getElementById("user-remove-button"), 
-  spaceRemoveButton =  document.getElementById("space-remove-button"), memberRemoveButton =  document.getElementById("member-remove-button"), 
-  channel = 'objects';
+  let userInput = document.getElementById("user-input"), spaceInput = document.getElementById("space-input"),
+    memberInputUsername = document.getElementById("member-input-username"), memberInputSpacename = document.getElementById("member-input-spacename"), 
+    userBox =  document.getElementById("user-box"), spaceBox =  document.getElementById("space-box"),
+    memberBox =  document.getElementById("member-box"), channel = 'objects';
 
+  let users = [];
+  let spaces = [];
+  let members = [];
+
+  // Init PubNub
   let pubnub = new PubNub({
     publishKey : 'INSERT_PUB_KEY',
     subscribeKey : 'INSERT_SUB_KEY',
@@ -18,17 +19,20 @@ let userInput = document.getElementById("user-input"), spaceInput = document.get
   pubnub.addListener({
     message: function(msg) {
       if(msg.message.userInput){
-        console.log(msg.message)
+        users.unshift(msg.message.userInput);
         userBox.innerHTML =  (''+msg.message.userInput)+ '<br>' + userBox.innerHTML;
       }
 
       else if(msg.message.spaceInput){
-        console.log(msg.message.spaceBox)
+        spaces.unshift(msg.message.spaceInput);
         spaceBox.innerHTML =  (''+msg.message.spaceInput)+ '<br>' + spaceBox.innerHTML;
       }
 
       else if(msg.message.memberInputSpacename){
-        console.log(msg.message)
+        members.unshift({
+          'space_name' : msg.message.memberInputSpacename,
+          'user_name' : msg.message.memberInputUsername,
+        });
         memberBox.innerHTML =  (''+msg.message.memberInputSpacename)+ ': ' + (''+msg.message.memberInputUsername) +'<br>' + memberBox.innerHTML;
       }
     }
@@ -76,5 +80,36 @@ let userInput = document.getElementById("user-input"), spaceInput = document.get
       alert('Enter both space and user field')
     }
   }
+
+  removeUser = () =>{
+    if(userBox.innerHTML.length === 0) return;
+    users = users.slice(1);
+    userBox.innerHTML = '';
+    for(let x = users.length - 1; x >= 0; x--){ // start from end of array
+			userBox.innerHTML = (''+ users[x]) + '<br>' + userBox.innerHTML;
+		}
+  }
+
+  removeSpace = () => {
+    if(spaceBox.innerHTML.length === 0) return;
+    spaces = spaces.slice(1);
+    spaceBox.innerHTML = '';
+    for(let x = spaces.length - 1; x >= 0; x--){ // start from end of array
+			spaceBox.innerHTML = (''+ spaces[x]) + '<br>' + spaceBox.innerHTML;
+		}
+  }
+
+  removeMember = () => {
+    members = members.slice(1);
+    memberBox.innerHTML = '';
+
+    for(let x = members.length - 1; x >= 0; x--){ // start from end of array
+      let obj = members[x];
+      let keyOne = obj[Object.keys(obj)[0]]; // get first value of object
+      let keyTwo = obj[Object.keys(obj)[1]]; // get second value of object
+			memberBox.innerHTML = (''+ keyOne) + ': ' + (''+ keyTwo) + '<br>' + memberBox.innerHTML;
+		}
+  }
+
 })();
   
